5a43ab978059eb97ebb37c734055e4a3693d18f9
Sanjoy <sanjoylaha94@gmail.com>
2025-10-18 00:00:59 +0530

    Add checkout page into React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes,Route } from 'react-router';
 import { HomePage } from './Pages/HomePage';
+import { CheckoutPage } from './Pages/CheckoutPage';
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={  <HomePage/>}></Route>
+      <Route path="checkout" element={<CheckoutPage/>}></Route>
     </Routes>
   
   )
