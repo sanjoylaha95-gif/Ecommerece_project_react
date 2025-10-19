f2a7cb10165ffb439db1e0ef8b9304455f3abdbb
Sanjoy <sanjoylaha94@gmail.com>
2025-10-19 23:44:25 +0530

    Generate HTML for the orders page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     <Routes>
       <Route path="/" element={  <HomePage cart={cart}/>}></Route>
       <Route path="checkout" element={<CheckoutPage cart={cart}/>}></Route>
-      <Route path="orders" element={<OrdersPage/>}></Route>
+      <Route path="orders" element={<OrdersPage cart={cart}/>}></Route>
     </Routes>
   
   )
